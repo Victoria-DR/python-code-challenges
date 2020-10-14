0b6e1fc804e60cef39c7dd5d01edd3225f48faee
DR-Victoria <68347113+DR-Victoria@users.noreply.github.com>
2020-10-14 16:00:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Python Code Challenges
+
+https://www.linkedin.com/learning/python-code-challenges/put-your-python-skills-to-the-test
+"""
+
+import prime_factors
+import palindrome
+import sort_words
+import index_all
